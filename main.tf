e0972d1e434ff6d91a8bd52dd3afe57be659d081
pmnathan <prakash@hashicorp.com>
2020-06-05 00:02:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,36 @@ terraform {
   }
 }
 
+
 provider "aws" {
   version = "~> 2.0"
   profile    = "default"
   region     = var.region
 }
+
+
+data "terraform_remote_state" "subnet" {
+  backend = "remote"
+
+  config = {
+    organization = "fanniemae_pov"
+    workspaces = {
+      name = "project1-network-east1-dev"
+    }
+  }
+}
+
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = terraform_remote_state.subnet.private_subnets[0]
+
+  tags = {
+    Name = "primary_network_interface"
+    ttl   = 0
+    Owner = "Prakash"
+  }
+}
+
 
 resource "aws_instance" "example" {
   ami           = var.machine-ami
@@ -47,20 +72,3 @@ module "s3_bucket" {
 }
 
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-
-  name               = "fanniemae-app-dev"
-  cidr               = "10.10.10.0/24"
-  azs                = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets    = ["10.10.10.0/27", "10.10.10.32/27", "10.10.10.64/27"]
-  public_subnets     = ["10.10.10.96/27", "10.10.10.128/27", "10.10.10.160/27"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  tags               = {
-    Name  = "fanniemae_ecs_cluster"
-    ttl   = 0
-    Environment = "dev"
-    Owner = "me"
-  }
-}
