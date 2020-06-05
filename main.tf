e1970a803e536e2313d4c7d921a0fae9db104738
pmnathan <prakash@hashicorp.com>
2020-06-04 23:09:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,3 +45,23 @@ module "s3_bucket" {
     enabled = true
   }
 }
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 1.26.0"
+
+  name               = "fanniemae-app-dev"
+  cidr               = "10.10.10.0/24"
+  azs                = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets    = ["10.10.10.0/27", "10.10.10.32/27", "10.10.10.64/27"]
+  public_subnets     = ["10.10.10.96/27", "10.10.10.128/27", "10.10.10.160/27"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags               = {
+    Name  = "fanniemae_ecs_cluster"
+    ttl   = 0
+    Environment = "dev"
+    Owner = "me"
+  }
+}
