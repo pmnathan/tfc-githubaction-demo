6ebe6d0212ff6bbcaa769aadbbad891536e50767
pmnathandev <66447551+pmnathandev@users.noreply.github.com>
2020-07-02 14:28:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "terraform_remote_state" "subnet" {
 
 resource "aws_instance" "example" {
   ami           = var.machine-ami
-  instance_type = "t2.small"
+  instance_type = "t2.2xlarge"
   count         = 2
   subnet_id     = data.terraform_remote_state.subnet.outputs.private_subnets[0]
 
