835c9214c1132af7244c2a49f5dc9680db466426
pmnathandev <66447551+pmnathandev@users.noreply.github.com>
2020-09-23 10:24:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ data "terraform_remote_state" "subnet" {
 resource "aws_instance" "example" {
   ami           = var.machine-ami
   instance_type = "t2.small"
-  count         = 2
+  count         = 4
   subnet_id     = data.terraform_remote_state.subnet.outputs.private_subnets[0]
   tags = {
     Name  = "prakash-demo-${count.index}"
